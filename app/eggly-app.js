cf1cdbce8d51ded1dff99ead9024cb4e8d0e78d1
Evgeny Rybakov <jinzito@gmail.com>
2016-07-10 11:06:30 +0300

    add one empty category to model

--- app/eggly-app.js
+++ app/eggly-app.js
@@ -9,7 +9,8 @@ angular.module('Eggly', [
             {"id": 0, "name": "Development"},
             {"id": 1, "name": "Design"},
             {"id": 2, "name": "Exercise"},
-            {"id": 3, "name": "Humor"}
+            {"id": 3, "name": "Humor"},
+			{"id": 4, "name": "Other"}
         ];
 
         $scope.bookmarks = [
